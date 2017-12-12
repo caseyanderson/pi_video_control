ce74f2ad420d08a221af68a2e9636e1b9b58e879
caseyanderson <cta@caseyanderson.com>
2017-12-12 14:25:29 -0800

    lasjdflkajs

--- video_player.py
+++ video_player.py
@@ -9,11 +9,11 @@ import subprocess
 import io
 
 class Player():
-    
+
     def __init__(self, path):     # path should be set during init
-        self.path = path  
+        self.path = path
         self.process = None
-    
+
     def loop(self):
         path = self.path
         self.process = subprocess.Popen(['omxplayer', '-b', '--no-osd', '--loop', path], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -21,22 +21,23 @@ class Player():
     def play(self):
         path = self.path
         self.process = subprocess.Popen(['omxplayer', '-b', '--no-osd', path], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+
     def status(self):
         if self.process.poll() is not None:
-            return 'done' 
+            return 'done'
         else:
-            return 'playing' 
+            return 'playing'
 
     def info(self):
         self.process.stdin.write(b'z')
         self.process.stdin.flush()
-        return self.process.stdout
-    
+        for line in io.TextIOWrapper(self.process, encoding="utf-8"):
+            print(line)
+
     def stop(self):         # is quit the same as terminate?
         self.process.stdin.write(b'q')
         self.process.stdin.flush()
-    
+
     def toggle(self):
         self.process.stdin.write(b'p')
         self.process.stdin.flush()
