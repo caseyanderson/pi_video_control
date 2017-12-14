dc59021c4413ea3b419deb237d973369e5356907
caseyanderson <cta@caseyanderson.com>
2017-12-13 16:53:59 -0800

    fixing typo

--- toggle_pause_loop.py
+++ toggle_pause_loop.py
@@ -38,6 +38,6 @@ except KeyboardInterrupt:
     button.close()
     if loop.status() is 'playing':
         print('video is playing, terminating now!')
-        os.killpg(os.getpgid(play_process.pid), signal.SIGTERM)  # Send the signal to all the process groups (found this here: https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true )
+        os.killpg(os.getpgid(loop.pid), signal.SIGTERM)  # Send the signal to all the process groups (found this here: https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true )
     else:
         print('no video running, exiting now')
