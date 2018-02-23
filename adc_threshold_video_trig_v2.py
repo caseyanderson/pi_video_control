897c7a3660daa731777e7451e09a2f09bd1eae52
caseyanderson <cta@caseyanderson.com>
2018-02-23 12:11:12 -0800

    second version

---
+++ adc_threshold_video_trig_v2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+'''
+trigger a video from button press or similar
+'''
+
+from video_player import *
+import time
+import Adafruit_ADS1x15
+
+BASE_DIR = '/home/pi/'
+FILENAME =  'dramatic_chipmunk.mp4'
+
+play_path = ''.join([BASE_DIR, FILENAME])
+
+is_playing = False
+
+play = Player(play_path)
+adc = Adafruit_ADS1x15.ADS1015()
+
+GAIN = 1
+threshold = 500
+
+try:
+    while True:
+        sensor_val = adc.read_adc(0, gain=GAIN)
+        if is_playing == False and sensor_val >= threshold:
+            print('threshold crossed, starting video')
+            play.play()
+            is_playing = True
+        elif is_playing == True:
+            if play.status() == 'done':
+                print(''.join(['done', '\n', '\n']))
+                is_playing = False
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    print(''.join([ '\n', '\n', 'INTERRUPTED', '\n']))
+    button.close()
+    if play.status() == 'playing':
+        print('video is running, terminating now!')
+        play.kill()
+    else:
+        print('no video running, exiting now')
