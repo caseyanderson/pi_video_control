4c0a5b6ee0236ec2f6fddad6264f2b567da9df90
caseyanderson <cta@caseyanderson.com>
2018-02-23 12:16:18 -0800

    blargh

--- adc_threshold_video_trig.py
+++ adc_threshold_video_trig.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-#threshold controls video playback
 
-import sys
+'''
+trigger a video from button press or similar
+'''
+
 from video_player import *
-
 import time
 import Adafruit_ADS1x15
 
@@ -11,27 +12,31 @@ BASE_DIR = '/home/cta/'
 FILENAME =  'dramatic_chipmunk.mp4'
 
 play_path = ''.join([BASE_DIR, FILENAME])
+
+is_playing = False
+
+play = Player(play_path)
 adc = Adafruit_ADS1x15.ADS1015()
 
 GAIN = 1
 threshold = 500
-is_playing = False
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
-
-while True:
-    sensor_val = adc.read_adc(0, gain=GAIN)
-    # print(''.join(['sensor val is ', str(sensor_val)]))
-    if is_playing == False and sensor_val >= threshold:
-        print('do nothing!')
-    elif is_playing == False and sensor_val < threshold:
-        print('play video')
-        play = Player(play_path)
-        time.sleep(0.01) # unnecessary
-        play.play()
-        is_playing = True
-        if play.status() == 'done':
-            print(''.join(['done', '\n', '\n']))
-            is_playing = False
-    # Pause for half a second.
-    time.sleep(0.1)
+try:
+    while True:
+        sensor_val = adc.read_adc(0, gain=GAIN)
+        if is_playing == False and sensor_val <= threshold:
+            print('threshold crossed, starting video')
+            play.play()
+            is_playing = True
+        elif is_playing == True:
+            if play.status() == 'done':
+                print(''.join(['done', '\n', '\n']))
+                is_playing = False
+except KeyboardInterrupt:
+    print(''.join([ '\n', '\n', 'INTERRUPTED', '\n']))
+    button.close()
+    if play.status() == 'playing':
+        print('video is running, terminating now!')
+        play.kill()
+    else:
+        print('no video running, exiting now')
