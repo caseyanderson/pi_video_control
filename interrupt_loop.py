2e725fdda3a4928fe7f8af0f709f08dbbf722ed6
caseyanderson <cta@caseyanderson.com>
2018-10-31 14:38:49 -0700

    added led

--- interrupt_loop.py
+++ interrupt_loop.py
@@ -8,14 +8,16 @@ arg1 is base directory
 arg2 is loop file to play
 arg3 is interruption file to play
 arg4 is which pin is the button
+arg5 is which pin is the LED
 
-TO RUN: python3 interrupt_loop.py /base/dir/ loopfile.mov intteruptfile.mov GPIOPIN
-i.e. python3 interrupt_loop.py /home/pi/ dramatic_chipmunk.webm head_explode.webm 16
+TO RUN: python3 interrupt_loop.py /base/dir/ loopfile.mov intteruptfile.mov BUTTON_PIN LED_PIN
+i.e. python3 interrupt_loop.py /home/pi/ dramatic_chipmunk.webm head_explode.webm 16 17
 
 '''
 
 from video_player import *
 from gpiozero import Button
+from gpiozero import LED
 from time import sleep
 import sys
 
@@ -23,8 +25,10 @@ BASE_DIR = str(sys.argv[1])
 LOOP_FILENAME =  str(sys.argv[2])
 INT_FILENAME =  str(sys.argv[3])
 BUTTON_PIN = int(sys.argv[4])
+LED_PIN = int(sys.argv[5])
 
 button = Button(BUTTON_PIN)
+led = LED(LED_PIN)
 
 first_time = 1
 interrupted = 0
@@ -35,7 +39,8 @@ play_path = ''.join([BASE_DIR, INT_FILENAME])
 v1_playing = 0
 v2_playing = 0
 
-print("ready!")
+#print("ready!")
+led.on()
 
 try:
     while True:
@@ -67,3 +72,4 @@ try:
 except KeyboardInterrupt:
     print(''.join([ '\n', '\n', 'INTERRUPTED', '\n']))
     button.close()
+    led.close()
